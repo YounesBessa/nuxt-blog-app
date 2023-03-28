c825d0155aad8ba8001e195e066375970be24b02
Younès Bessa <bessa460@gmail.com>
2023-03-29 01:43:15 +0200

    🔧 update nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Blogging',
+    title: 'nuxt-blog-app',
     htmlAttrs: {
       lang: 'en',
     },
