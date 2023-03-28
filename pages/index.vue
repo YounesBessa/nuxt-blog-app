e0b6895b14834c2e79c0f83bed24f965d342d441
Younès Bessa <bessa460@gmail.com>
2023-03-29 01:37:37 +0200

    ✨ add home page

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page page-home">
+      <h1 class="text-3xl font-bold">Accueil</h1>
+      <div class="flex flex-row gap-2">
+          <NuxtLink to="/article">Liste des articles</NuxtLink>
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
